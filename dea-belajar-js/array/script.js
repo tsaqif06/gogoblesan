2fb54698fa0a1e8742d410bccb60592842510f58
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2023-02-10 21:59:37 +0700

    tugas array deaafrizal

---
+++ dea-belajar-js/array/script.js
@@ -0,0 +1,9 @@
+const arr = [4, 6, 8, "*", 12, 14];
+
+const cekBintang = arr.includes("*");
+const indexBintang = arr.indexOf("*");
+const bintang = arr[indexBintang];
+
+console.log(cekBintang);
+console.log(indexBintang);
+console.log(bintang);
